fcc0ff6b3c0767be9746b47dfd3a5b3168a3422c
JuanDHNZ <juandavid.hernandez@utp.edu.co>
2021-07-25 09:31:01 -0500

    Remake of searchOnKAF funtionalities (unfinished)

---
+++ tunning.py
@@ -0,0 +1,28 @@
+import pandas as pd  
+import numpy as np
+from tqdm import tqdm
+
+# datasets
+from datasets.ChaoticTimeSeries import GenerateAttractor
+from datasets.TestingSystems import GenerateSystem
+
+# models and metrics
+import KAF
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+attractors = ["chua","lorenz","duffing","nose_hoover","rikitake","rossler","wang"]
+nonlinears = ["4.1_AKB", "4.2_AKB"]
+def GridSearchKAF(KAF, grid, testingSystem,n_samples,trainSplit,MC_runs):
+    # 1. Generar Datos
+    if testingSystem in attractors:
+        
+    elif testingSystem in nonlinears:
+        system = GenerateSystem(samples=5003, systemType="4.2_AKB")
+    
+    X,y = Embedder(X=system, embedding=2)
+    Xtrain, Xtest, ytrain, ytest = TrainTestSplit(X,y)
+
+    # 2. Filter 
+
+    return
