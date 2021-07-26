2a23dd17c8702bb4adbf1f167f2a0555d38e7759
JuanDHNZ <juandavid.hernandez@utp.edu.co>
2021-07-26 08:13:43 -0500

    Testing MSE calculation and plotting

--- benchmark.py
+++ benchmark.py
@@ -9,7 +9,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-# 1. data generation
+import pandas as pd
+def get_kaf_params(filename):
+    param_path = "results/4.2/"
+    params = pd.read_csv(param_path + filename)
+    best_params = params[params.tradeOff_dist == params.tradeOff_dist.min()].iloc[0]
+    return best_params
+
+def MSE(y_true, y_pred):
+    import numpy as np
+    return np.mean((y_true-np.array(y_pred).reshape(-1,1))**2/y_true**2)
+
+def filter_evaluation(filt):
+    results = {"TMSE":[],"CB":[]}
+    from tqdm import tqdm
+    for xi, yi in tqdm(zip(Xtrain,ytrain)):
+        try:
+            #train and predict
+            filt.evaluate(xi,yi)
+            y_pred = filt.predict(Xtest)
+            #scoring
+            results["CB"].append(len(filt.CB))
+            results["TMSE"].append(MSE(ytest,y_pred))
+        except:
+            results["CB"].append(0)
+            results["TMSE"].append(0)
+    return results
+
+#%% 1. data generation
 from datasets.ChaoticTimeSeries import GenerateAttractor
 from datasets.TestingSystems import GenerateSystem
 from datasets.tools import Embedder, TrainTestSplit
@@ -18,46 +45,49 @@ system = GenerateSystem(samples=5003, systemType="4.2_AKB")
 X,y = Embedder(X=system, embedding=2)
 Xtrain, Xtest, ytrain, ytest = TrainTestSplit(X,y)
 
-# 2. Parameter selection 
-import pandas as pd
-def get_kaf_params(filename):
-    param_path = "results/4.2/"
-    params = pd.read_csv(param_path + filename)
-    best_params = params[params.tradeOff_dist == params.tradeOff_dist.min()].iloc[0]
-    return best_params
-
+#%% 2. Parameter selection 
 params_qklms = get_kaf_params("QKLMS_4.2_5000.csv")
 params_akb = get_kaf_params("QKLMS_AKB_4.2_5000.csv")
 params_amk = get_kaf_params("QKLMS_AMK_4.2_5000.csv")
 
-# 3. QKLMS results
+#%% 3. QKLMS results
 from KAF import QKLMS
 filt = QKLMS(eta=params_qklms.eta,
              epsilon=params_qklms.epsilon,
              sigma=params_qklms.sigma)
 
-def MSE(y_true, y_pred):
-    return np.mean((y_true-np.array(y_pred).reshape(-1,1))**2/y_true**2)
+print("QKLMS TMSE...\n")
+results = filter_evaluation(filt)
+savename = "results/4.2/TMSE_QKLMS_4.2.csv"
+pd.DataFrame(data=results).to_csv(savename)
+print("QKLMS saved in {}\n\n".format(savename))
 
-print("QKLMS TMSE...")
-TMSE = []
-CB = []
-for xi, yi in tqdm(zip(Xtrain,ytrain)):
-    #train and predict
-    filt.evaluate(xi,yi)
-    y_pred = filt.predict(Xtest)
-    #scoring
-    CB.append(len(filt.CB))
-    TMSE.append(MSE(ytest,y_pred))
+#%% 3. QKLMS_AKB results
+from KAF import QKLMS_AKB
+filt = QKLMS_AKB(eta=params_akb.eta,
+             epsilon=params_akb.epsilon,
+             sigma_init=params_akb.sigma_init,
+             mu=params_akb.mu,
+             K=int(params_akb.K))
 
-    
+print("QKLMS AKB TMSE...\n")
+results = filter_evaluation(filt)
+savename = "results/4.2/TMSE_QKLMS_AKB_4.2.csv"
+pd.DataFrame(data=results).to_csv(savename)
+print("QKLMS AKB saved in {}\n\n".format(savename))
 
+#%% 3. QKLMS_AMK results
+from KAF import QKLMS_AMK
+filt = QKLMS_AKB(eta=params_amk.eta,
+             epsilon=params_amk.epsilon,
+             mu=params_amk.mu,
+             K=int(params_amk.K))
 
-
-
-
-
-
+print("QKLMS AMK TMSE...\n")
+results = filter_evaluation(filt)
+savename = "results/4.2/TMSE_QKLMS_AMK_4.2.csv"
+pd.DataFrame(data=results).to_csv(savename)
+print("QKLMS AMK saved in {}\n\n".format(savename))
 
 
 
--- results_plotter.py
+++ results_plotter.py
@@ -8,20 +8,20 @@ sns.set()
 
 path = "results/4.2/"
 ## a. Testing MSE plot
-QKLMS = pd.read_csv(path + "testingMSE_QKLMS_4000-1000_42.csv")
-AKB = pd.read_csv(path + "testingMSE_QKLMS_AKB_4000-1000_42.csv")
-AMK = pd.read_csv(path + "testingMSE_QKLMS_AMK_4000-1000_42.csv")
+QKLMS = pd.read_csv(path + "TMSE_QKLMS_4.2.csv")
+AKB = pd.read_csv(path + "TMSE_QKLMS_AKB_4.2.csv")
+AMK = pd.read_csv(path + "TMSE_QKLMS_AMK_4.2.csv")
 
 plt.figure(figsize=(10,8))
 plt.yscale("log")
 plt.ylim((1e-30,1e1))
 plt.ylabel("TMSE")
 plt.xlabel("iterations")
-plt.plot(np.linspace(0,4000,800),QKLMS.MSE, color="magenta",label="QKLMS", lw=3, marker="*", ms=10,markevery=20)
-plt.plot(np.linspace(0,4000,800),AKB.MSE, color="cyan",label="QKLMS_AKB", lw=3, marker="8", ms=10,markevery=20)
-plt.plot(np.linspace(0,4000,800),AMK.MSE, color="lightgreen",label="QKLMS_AMK", lw=3, marker=9, ms=10,markevery=20)
+plt.plot(np.linspace(0,4000,800),QKLMS.TMSE, color="magenta",label="QKLMS", lw=3, marker="*", ms=10,markevery=20)
+plt.plot(np.linspace(0,4000,800),AKB.TMSE, color="cyan",label="QKLMS_AKB", lw=3, marker="8", ms=10,markevery=20)
+plt.plot(np.linspace(0,4000,800),AMK.TMSE, color="lightgreen",label="QKLMS_AMK", lw=3, marker=9, ms=10,markevery=20)
 plt.legend()
-plt.savefig(path + 'TMSE.png', dpi=300)
+plt.savefig(path + 'TMSE2.png', dpi=300)
 plt.show()
 
 
