f67814012b735e100165481c3f9fb35efa774d31
JuanDHNZ <juandavid.hernandez@utp.edu.co>
2021-07-26 14:12:22 -0500

    New grid search function implemented

--- tunning.py
+++ tunning.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 # datasets
 from datasets.ChaoticTimeSeries import GenerateAttractor
 from datasets.TestingSystems import GenerateSystem 
-import datasets.tools
+from datasets.tools import *
 
 # models and metrics
 import KAF
@@ -15,20 +15,82 @@ from sklearn.metrics import mean_squared_error
 attractors = ["chua","lorenz","duffing","nose_hoover","rikitake","rossler","wang"]
 nonlinears = ["4.1_AKB", "4.2_AKB"]
 
-def GridSearchKAF(KAF, grid, testingSystem,n_samples,trainSplit,MC_runs):
-    # 1. Generar Datos
+def GridSearchKAF(filt, grid, testingSystem, n_samples,MC_runs):
+    n_samples = 5003
+    testingSystem = "4.2_AKB"
+    filt = "QKLMS"
+    # 1. data generation
     if testingSystem in attractors:
         
     elif testingSystem in nonlinears:
-        system = GenerateSystem(samples=5003, systemType="4.2_AKB")
+        system = GenerateSystem(samples=n_samples, systemType=testingSystem)
         system = tools.z_scorer(system)
     else:
-        raise ValueError("{} dataset is not supported".format(testingSystem))   
-    
+        raise ValueError("{} dataset is not supported".format(testingSystem))
+
+    # 2. data preparation
     X,y = Embedder(X=system, embedding=2)
     Xtrain, Xtest, ytrain, ytest = TrainTestSplit(X,y)
 
-    # 2. Filter 
+    # 3. parameter grid 
+    params = parameter_picker(filt, grid)
     
-
+    # 4. grid evaluation
+    results = []
+    for params in tqdm(grid):
+        try:
+            f = KAF_picker(filt, params)
+            y = f.evaluate(Xtrain,ytrain)
+            ypred = f.predict(Xtest)
+            err = ytest-ypred.reshape(-1,1)
+            p['TMSE'] = np.mean(err**2)
+            p['CB'] = len(f.CB)
+            p['tradeOff'] = tradeOff(p['TMSE'],p['CB']/n_train)
+        except:
+            p['TMSE'] = np.nan
+            p['CB'] = np.nan
+            p['tradeOff'] = np.nan
+        results.append(p)
+        pd.DataFrame(data=results).to_csv(savename)
     return
+
+
+def GridSearchKAF_MC(filt, grid, testingSystem, n_samples,MC_runs):
+    n_samples = 5003
+    testingSystem = "4.2_AKB"
+    filt = "QKLMS"
+    # 1. data generation
+    if testingSystem in attractors:
+        
+    elif testingSystem in nonlinears:
+        system = GenerateSystem(samples=n_samples, systemType=testingSystem)
+        system = tools.z_scorer(system)
+    else:
+        raise ValueError("{} dataset is not supported".format(testingSystem))
+
+    # 2. data preparation
+    X,y = Embedder(X=system, embedding=2)
+    Xtrain, Xtest, ytrain, ytest = TrainTestSplit(X,y)
+
+    # 3. parameter grid 
+    params = parameter_picker(filt, grid)
+    
+    # 4. grid evaluation
+    results = []
+    for params in tqdm(grid):
+        try:
+            f = KAF_picker(filt, params)
+            y = f.evaluate(Xtrain,ytrain)
+            ypred = f.predict(Xtest)
+            err = ytest-ypred.reshape(-1,1)
+            p['TMSE'] = np.mean(err**2)
+            p['CB'] = len(f.CB)
+            p['tradeOff'] = tradeOff(p['TMSE'],p['CB']/n_train)
+        except:
+            p['TMSE'] = np.nan
+            p['CB'] = np.nan
+            p['tradeOff'] = np.nan
+        results.append(p)
+        pd.DataFrame(data=results).to_csv(savename)
+    return
+
