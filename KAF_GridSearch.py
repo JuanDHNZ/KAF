3cca147729add588d53e4c6e53d2cee3303bc284
JuanDHNZ <juandavid.hernandez@utp.edu.co>
2021-07-27 16:26:26 -0500

    Grid search executable

--- KAF_GridSearch.py
+++ KAF_GridSearch.py
@@ -10,12 +10,14 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--kaf', help='Filter to train')
 parser.add_argument('--dataset', help='Dataset to use')
 parser.add_argument('-samples', help='Dataset length',default=1000,type=int)
+parser.add_argument('-mc_runs', help='Monte Carlo runs',default=50,type=int)
 parser.add_argument('--savepath', help='path where to save results')
 
 args = parser.parse_args()
 kaf = args.kaf
 dataset = args.dataset
 samples = args.samples
+mc_runs = args.mc_runs
 savepath = args.savepath
 
 #%% For testing only:
@@ -24,19 +26,17 @@ savepath = args.savepath
 # dataset = "4.2_AKB"    
 # samples = 23
 # savepath = "results/4.2/New/"
-
+# mc_runs = 5
 #%%
 def main():
     from datasets.tools import grid_picker
     grid  = grid_picker(kaf)
     
-    from tunning import GridSearchKAF
-    GridSearchKAF(filt=kaf,grid=grid,testingSystem=dataset,n_samples=samples,savepath=savepath)
+    from tunning import  GridSearchKAF, GridSearchKAF_MC
+    # GridSearchKAF(filt=kaf,grid=grid,testingSystem=dataset,n_samples=samples,savepath=savepath)
+    GridSearchKAF_MC(filt=kaf,grid=grid,testingSystem=dataset,n_samples=samples,mc_runs=mc_runs,savepath=savepath)
 
     
 if __name__ == "__main__":
     main()
 
-
-# from test_on_KAF import kafSearch_MC
-# kafSearch_MC("QKLMS", "wang", 100, 0.8,10)
