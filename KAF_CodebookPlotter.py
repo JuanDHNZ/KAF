620e164dc742cef3346566a157ef21512ccea6a3
JuanDHNZ <juandavid.hernandez@utp.edu.co>
2021-07-29 16:45:45 -0500

    Codebook grafication executable

---
+++ KAF_CodebookPlotter.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 29 10:36:47 2021
+
+@author: USUARIO
+"""
+
+from testing import CB_visualizer
+ts = '4.2_AKB'
+samples = 2000
+p_file = "results/4.2/New/mc_QKLMS_AMK_4.2_AKB_5003.csv"
+model = CB_visualizer(testingSystem=ts, n_samples=samples, params_file = p_file)
